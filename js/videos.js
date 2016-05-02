98aae3a4ad819299f995c1d9f0ebc57a3808565f
amaliacardenas <amaliacardenas@gmail.com>
2016-05-02 19:05:41 +0200

    added sorting functionality to video portfolio

---
+++ js/videos.js
@@ -0,0 +1,14 @@
+var $boxs = $(".box");
+var $btns = $(".btn").on("click", function() {
+  
+  var active = 
+    $btns.removeClass("active")
+      .filter(this)
+      .addClass("active")
+      .data("filter"); 
+  $boxs
+    .hide()
+    .filter( "." + active )
+    .fadeIn(450);
+
+});
